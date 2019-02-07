82bb9d2141dfa2ae2a1064f5c36efdd065f41f9c
afreenbegumk <42670676+afreenbegumk@users.noreply.github.com>
2019-02-07 21:33:53 +0530

    Create evenorodd.java

---
+++ evenorodd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class oddoreven
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int x;
+System.out.println=("enter an integer to check whether it even or odd");
+Scanner in=new Scaner(System.in);
+if (x%2==0)
+System.out.println("even".);
+else
+System.out.println("odd".);
+
+	}
+}
