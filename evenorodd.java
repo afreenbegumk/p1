282fe6662a78e52c50704b73cb310f737a27742d
afreenbegumk <42670676+afreenbegumk@users.noreply.github.com>
2019-02-07 22:22:03 +0530

    Update evenorodd.java

--- evenorodd.java
+++ evenorodd.java
@@ -10,10 +10,10 @@ System.out.println=("enter an integer to check whether it even or odd");
 Scanner in=new Scaner(System.in);
 		x=in.nextInt();
 if (x%2==0)
-System.out.println("even".);
+System.out.println("even",x);
 elif
-System.out.println("odd".);
+System.out.println("odd",x);
 else
-	System.out.println("invalid".);
+	System.out.println("invalid",x);
 	}
 }
