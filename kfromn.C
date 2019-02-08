3fe56c22d0c2167d2983ea20fe62f651b996dfed
afreenbegumk <42670676+afreenbegumk@users.noreply.github.com>
2019-02-08 11:24:14 +0530

    Create kfromn.C

---
+++ kfromn.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n, k, i;
+    printf("Enter the two nos ");
+    scanf("%d %d", &n, &k);
+printf("enter the array of nos ");
+scanf("%d",&n);
+for(k=1;k<i;++i)
+printf("%d ",++i);
+return(0);
+}
