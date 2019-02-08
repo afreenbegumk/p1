6e07ed89762892f4e145dd091d1c8fbd74b44694
afreenbegumk <42670676+afreenbegumk@users.noreply.github.com>
2019-02-08 09:00:23 +0530

    Create vowelconsonant.C

---
+++ vowelconsonant.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+char str;
+printf("enter the characters");
+if ('a','e','i','o','u')
+printf("the given character is vowel");
+else
+printf("the given character is consonant");
+return 0;
+}
