8c2532d2ae423a3fc66ff74cc5c8ecb2c03b16df
afreenbegumk <42670676+afreenbegumk@users.noreply.github.com>
2019-02-08 11:30:50 +0530

    Create totaldigit.C

---
+++ totaldigit.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+long long num;
+int count = 0;
+printf("Enter any number: ");
+scanf("%lld", &num);
+count = log10(num) + 1;
+ printf("Total digits: %d", count);
+ return 0;
+}
